77682f953765a13e616ecc6bd545067923cddd8a
Mr-TS-1998 <abhishekts.clt@gmail.com>
2023-03-20 13:27:10 +0530

    Initial commit

---
+++ bin/sample2.dart
@@ -0,0 +1,11 @@
+void main()
+{
+  // datatype variable name = value
+  String name="Mr TS";
+  int age=24;
+  double mark=12.5+5;
+
+  print("my name $name");
+  print("my age $age");
+  print("my mark $mark");
+}
---
+++ bin/sample3.dart
@@ -0,0 +1,24 @@
+class FileName {
+  String name = "Mr TS";
+  int age = 24;
+  double mark = 12.5 + 5;
+  static String course = "flutter";
+}
+
+void main() {
+  FileName obj = FileName();
+  print("My name is ${obj.name}");
+  print(obj.age);
+  print(obj.mark);
+
+  FileName obj1=FileName();
+  print("My name is ${obj1.name = "ammu"}");
+  print(obj1.age = 21);
+  print(obj1.mark = 12.6);
+  print(FileName.course);
+
+  print("My name is ${obj.name = "swathy"}");
+  print(obj.age = 25);
+  print(obj.mark = 11.8);
+  print(FileName.course);
+}
