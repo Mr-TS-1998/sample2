15accf7b1df53f3b2b33d71dfece6f15a179a8b6
Mr-TS-1998 <abhishekts.clt@gmail.com>
2023-03-23 12:51:58 +0530

    new files added

---
+++ bin/relational_operators.dart
@@ -0,0 +1,12 @@
+void main()
+{
+  dynamic age=24;
+
+  print('$age > 25 is ${age>25}');
+  print('$age < 25 is ${age<25}');
+  print('$age = 25 is ${age==25}');
+  print('$age != 25 is ${age!=25}');
+  print('$age >= 25 is ${age>=25}');
+  print('$age <= 25 is ${age<=25}');
+
+}
