cdd2d6a0aa87ab47ca938fcec7d2bfc13a309c40
Mr-TS-1998 <abhishekts.clt@gmail.com>
2023-03-23 12:42:39 +0530

    new files added

---
+++ bin/assignment_operators.dart
@@ -0,0 +1,11 @@
+void main()
+{
+  dynamic a=10, b=15;
+
+  print('a+=b = ${a+=b}');// a=a+b
+  print('a-=b = ${a-=b}');// a=a-b
+  print('a*=b = ${a*=b}');// a=a*b
+  print('a/=b = ${a/=b}');// a=a/b
+  print('a~/=b = ${a~/b}');// a=a~/b
+  print('a%=b = ${a%=b}');// a=a%b
+}
