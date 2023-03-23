f630ddc7881a0399783550fe0f2d1d552b2c1679
Mr-TS-1998 <abhishekts.clt@gmail.com>
2023-03-23 13:06:26 +0530

    new files added

---
+++ bin/uniary_operators.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  dynamic x=0;
+
+  print(++x);
+  print(++x);
+  print(++x);
+  print(++x);
+  print(++x);//5
+  print(x--);//5
+  print(x--);
+  print(x--);
+  print(x--);
+  print(x--);
+
+}
