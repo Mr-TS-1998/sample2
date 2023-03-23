511ab1da091e34706be8fe78dfdbb370186449b1
Mr-TS-1998 <abhishekts.clt@gmail.com>
2023-03-23 12:16:39 +0530

    new files added

---
+++ bin/arithmatic_operators.dart
@@ -0,0 +1,12 @@
+void main()
+{
+  dynamic a=20, b=25;
+
+      print('$a + $b = ${a+b}');
+      print('$a - $b = ${a-b}');
+      print('$a X $b = ${a*b}');
+      print('$a / $b = ${a/b}');
+      print('$a ~/ $b = ${a~/b}');
+      print('$a % $b = ${a%b}');
+      print('-($a + $b) = ${-(a+b)}');
+}
