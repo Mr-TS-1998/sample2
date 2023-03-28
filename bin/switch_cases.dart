a462377d97cc9b5ca943f139a99306d3b04a862d
Mr-TS-1998 <abhishekts.clt@gmail.com>
2023-03-28 12:54:36 +0530

    new

---
+++ bin/switch_cases.dart
@@ -0,0 +1,53 @@
+void main()
+{
+  var size='l';
+
+  switch(size)
+  {
+    case's':print("Size is small");
+    break;
+
+    case'm':print("Size is medium");
+    break;
+
+    case'l':print("Size is large");
+    break;
+
+    case'xl':print("Size is extra large");
+    break;
+
+    case'xxl':print("Size is super extra large");
+    break;
+
+  }
+
+  print("***************");
+
+  var day=5;
+
+  switch(day)
+  {
+    case 1:print("SUNDAY");
+    break;
+
+    case 2:print("MONDAY");
+    break;
+
+    case 3:print("TUESDAY");
+    break;
+
+    case 4:print("WEDNESDAY");
+    break;
+
+    case 5:print("THURSDAY");
+    break;
+
+    case 6:print("FRIDAY");
+    break;
+
+    case 7:print("SATURDAY");
+    break;
+
+  }
+
+}
