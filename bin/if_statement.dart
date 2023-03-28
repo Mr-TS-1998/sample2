f25d89bfdbb8570f3c7c9b961d774979391626f3
Mr-TS-1998 <abhishekts.clt@gmail.com>
2023-03-28 11:39:54 +0530

    new

---
+++ bin/if_statement.dart
@@ -0,0 +1,31 @@
+void main()
+{
+  int a=30;
+  String char='ABCD';
+  if(a>50)
+    {
+      print("$a is greater than 50");
+    }
+  print("False");
+  if(a>20)
+    {
+      print("$a is greater than 20");
+    }
+  print("False 2");
+  if(char=='ABCD')
+    {
+      print("True");
+    }
+  print("Error");
+
+  print("**************************");
+
+  var name='Admin', pwd='12345';
+  if(name=='Admim' && pwd=='12345') {
+    print("Login Successful");
+  }
+  else {
+    print("Invalid Login");
+  }
+
+}
