6eab37e3783beeb766ab550d6d3654f7f38324cd
Mr-TS-1998 <abhishekts.clt@gmail.com>
2023-03-28 12:27:27 +0530

    new

---
+++ bin/elseif_ladder.dart
@@ -0,0 +1,29 @@
+void main()
+{
+  var size='l';
+
+  if(size=='s')
+    {
+      print("Size is small");
+    }
+  else if(size=='m')
+    {
+      print("Size is medium");
+    }
+  else if(size=='l')
+    {
+      print("Size is large");
+    }
+  else if(size=='xl')
+    {
+      print("Size is extra large");
+    }
+  else if(size=='xxl')
+  {
+    print("Size is super extra large");
+  }
+  else
+    {
+      print("Invalid Size");
+    }
+}
