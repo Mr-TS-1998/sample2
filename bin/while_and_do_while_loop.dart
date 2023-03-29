970086b77d355cbe93a6241b86d32a57cfc2e735
Mr-TS-1998 <abhishekts.clt@gmail.com>
2023-03-29 12:42:45 +0530

    new

---
+++ bin/while_and_do_while_loop.dart
@@ -0,0 +1,36 @@
+/* WHILE LOOP SYNTAX
+
+  while(condition)
+  {
+  text expression;
+  body of loop
+  }
+ */
+
+/* DO WHILE SYNTAX
+  do
+  {
+    increment/decrement;
+  } while(condition);
+
+ */
+
+// void main()
+// {
+//   var a=1, max=10;
+//   do
+//     {
+//       print("$a");
+//       a=a+1;
+//     }while(a<max);
+// }
+
+void main()
+{
+  var a=1,max=10;
+  while(a<max)
+    {
+      print("$a");
+      a=a+1;
+    }
+}
