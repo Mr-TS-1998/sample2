21b68c209e95de2444763e1b5512513f69af73b6
Mr-TS-1998 <abhishekts.clt@gmail.com>
2023-03-29 12:14:27 +0530

    new

---
+++ bin/for_loops.dart
@@ -0,0 +1,107 @@
+// void main()
+// {
+//
+//   for(int i=1;i<=10;i++)
+//     {
+//       print(i);
+//     }
+// }
+
+
+// EVEN NO.
+// void main()
+// {
+//
+//   for(int i=1;i<=10;i++)
+//     {
+//       if(i%2==0)
+//         {
+//           print(i);
+//         }
+//     }
+// }
+
+
+//ODD NO.
+// void main()
+// {
+//
+//   for(int i=1;i<=10;i++)
+//   {
+//     if(i%2!=0)
+//     {
+//       print(i);
+//     }
+//   }
+// }
+
+
+// SUM UP TO GIVEN NO.
+// import 'dart:io';
+//
+// void main()
+// {
+//   int sum=0;
+//   print("Enter a number: ");
+//   int num=int.parse(stdin.readLineSync()!);
+//   for(int i=1;i<=num;i++)
+//     {
+//       sum=sum+i;
+//     }
+//   print("Total sum = $sum");
+// }
+
+
+// MULTIPLICATION
+// import 'dart:io';
+//
+// void main()
+// {
+//   print("Enter a number for multiplication: ");
+//   int num=int.parse(stdin.readLineSync()!);
+//   for(int i=1;i<=10;i++)
+//     {
+//       // int multi;
+//       // multi=num*i;
+//       print("$num X $i = ${num*i}");
+//     }
+// }
+
+
+// SUM OF EVEN NO.
+// import 'dart:io';
+// void main()
+// {
+//   int sum=0;
+//   print("Enter a even number: ");
+//   int num=int.parse(stdin.readLineSync()!);
+//   for(int i=1;i<=num;i++)
+//     {
+//       if(i%2==0)
+//         {
+//           sum=sum+i;
+//         }
+//
+//     }
+//   print("Total sum of even numbers are $sum");
+// }
+
+
+//SUM OF ODD NO.
+// import 'dart:io';
+// void main()
+// {
+//   int sum=0;
+//   print("Enter a odd number: ");
+//   int num=int.parse(stdin.readLineSync()!);
+//   for(int i=1;i<=num;i++)
+//   {
+//     if(i%2!=0)
+//     {
+//       sum=sum+i;
+//     }
+//
+//   }
+//   print("Total sum of odd numbers are $sum");
+// }
+
